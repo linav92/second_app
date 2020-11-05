0d69c9243e795fff70f198ea9dd6ed6fde6f66ab
Lina Vallejo <linav92@gmail.com>
2020-11-05 18:24:48 -0300

    Edit Aplication.html.erb to template

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  "users/login"
   get  "pages/about_us"
-  get "pages/test"
+  get  "pages/test"
   root "pages#index"
 end
